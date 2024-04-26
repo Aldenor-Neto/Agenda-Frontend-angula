ab7ad3c7416b114489f20c549cc891c0256bcef2
aldenor-neto <aldenor05062010@gmail.com>
2024-04-26 15:48:19 -0300

    cabeçario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { CriarContatoComponent } from './componentes/criar-contato/criar-contato.component';
 import { EditarContatoComponent } from './componentes/editar-contato/editar-contato.component';
 import { ListarContatosComponent } from './componentes/listar-contatos/listar-contatos.component';
+import { CabecarioComponent } from './componentes/cabecario/cabecario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CriarContatoComponent, EditarContatoComponent, ListarContatosComponent],
+  imports: [CommonModule, RouterOutlet, CriarContatoComponent, EditarContatoComponent, ListarContatosComponent, CabecarioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/componentes/cabecario/cabecario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecario',
+  standalone: true,
+  imports: [],
+  templateUrl: './cabecario.component.html',
+  styleUrl: './cabecario.component.css'
+})
+export class CabecarioComponent {
+
+}
