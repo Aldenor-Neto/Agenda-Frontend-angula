04b4a767f5a4f94592062538fc156e0f2112b29e
aldenor-neto <aldenor05062010@gmail.com>
2024-04-26 16:10:52 -0300

    rodapé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { CriarContatoComponent } from './componentes/criar-contato/criar-contato
 import { EditarContatoComponent } from './componentes/editar-contato/editar-contato.component';
 import { ListarContatosComponent } from './componentes/listar-contatos/listar-contatos.component';
 import { CabecarioComponent } from './componentes/cabecario/cabecario.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CriarContatoComponent, EditarContatoComponent, ListarContatosComponent, CabecarioComponent],
+  imports: [CommonModule, RouterOutlet, CriarContatoComponent, EditarContatoComponent, ListarContatosComponent, CabecarioComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
