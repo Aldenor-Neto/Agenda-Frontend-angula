bc5684ad540a1fea0b5839f570906fa30ec7badf
aldenor-neto <aldenor05062010@gmail.com>
2024-04-29 20:19:06 -0300

    filtro de pesquisa

--- src/app/componentes/listar-contatos/listar-contatos.component.ts
+++ src/app/componentes/listar-contatos/listar-contatos.component.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import { Contato } from '../contato/contato';
 import { ContatoService } from '../contato/contato.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-listar-contatos',
   standalone: true,
-  imports: [RouterLink, HttpClientModule],
+  imports: [RouterLink, HttpClientModule, FormsModule],
   providers: [ContatoService],
 
   templateUrl: './listar-contatos.component.html',
@@ -15,14 +16,26 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class ListarContatosComponent implements OnInit {
 
+  listaGeral: Contato[] = [];
   lista: Contato[] = [];
+  busca: string = "";
 
   constructor(private service: ContatoService) { }
 
   ngOnInit(): void {
     this.service.listar().subscribe((lista) => {
-      this.lista = lista;
+      this.listaGeral = lista;
     });
+    this.filtrarContatos();
+  }
+
+  filtrarContatos() {
+    this.listaGeral.sort((a, b) => a.nome > b.nome ? 1 : -1);
+    if (this.busca) {
+      this.lista = this.listaGeral.filter(contato => contato.nome.toLowerCase().includes(this.busca.toLowerCase()));
+    } else {
+      this.lista = this.listaGeral;
+    }
   }
 
 }
